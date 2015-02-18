4280c2f87e41af0a8450c40fa479f8ec02df9bfe
Manop Kongoon <kongoon@hotmail.com>
2015-02-18 11:59:41 +0700

    Commit ครั้งแรก
    การ Commit ครั้งแรก เพื่อสร้างโครงสร้างของระบบ HDC

---
+++ frontend/config/main-local.php
@@ -0,0 +1,21 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'J2DCOnzn5u9MFwE9hTBIpjr8v_xopKRI',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = 'yii\debug\Module';
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = 'yii\gii\Module';
+}
+
+return $config;
